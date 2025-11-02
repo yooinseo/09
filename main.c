0cb75adde5e6ed5400be565b0e5e38454b2abbc8
yooinseo <qetu37@sookmyung.ac.kr>
2025-11-02 15:04:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,19 +5,19 @@
 int main(int argc, char *argv[])
 {
     int i;
-    int grade[SIZE];
-    int score[SIZE];
+    int a[SIZE] = {1,2,3,4,5};
+    int b[SIZE] = {1,2,3,4,6};
+    int flag_same = 1;
     
     for(i=0;i<SIZE;i++)
     {
-      grade[i] = rand()%100;
+      if (a[i] != b[i])
+      {
+               printf("%i-th element is diff.\n", i);
+               flag_same = 0;
+      }
     } 
-    for(i=0;i<SIZE;i++)
-    {
-      score[i] = grade[i];
-    }
-    for(i=0;i<SIZE;i++)
-      printf("grade[%i] = %i, score[%i] = %i\n", i, grade[i], i, score[i]);
+    printf("Are a and b the same? %i\n", flag_same);
     
   system("PAUSE");	
   return 0;
